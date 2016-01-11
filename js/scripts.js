972a32b24dfd5e5360833f908cbc2cd29bb819c5
David Remington and Nicholas Jensen-Hay <david.ian.remington@gmail.com>
2016-01-11 14:54:11 -0800

    Init array and variables

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(function(){
+  window.flashcards = [
+   ["JavaScript", "A powerful browser based scripting language"],
+   ["Modulo", "%, the remainder of x/y"],
+   ["Operators", "Mathematical operators, such as, +, -, /, *, %, ^, and =="],
+   ["NaN", "Not a Number, but it is a type of number!"],
+   ["Camel Capitalization", "aWayToNameVariablesWithoutSpacesButCapitalizingTheFirstLetterOfEveryWord"],
+   ["+=", "Add the right to the left, instead of = which replaces the left with the right"],
+   ["Function", "A fancy way to say 'Run this block of text'"],
+   ["Method", "A piece of code performed on an object"],
+   ["Argument", "Information input into a method or function"],
+   ["String", "a group of alphanumeric characters contained in quotation marks"],
+   ["Chaining methods", "performing multiple methods on the same object in a single expression"],
+   ["Escape", "a character that causes JavaScript to ignore special characters"],
+   ["Alert", "a function that creates a dialogue box containing a string from an argument"],
+   ["Commenting code", "specially marked lines, ignored by the compiler, that make it easier for code to be read by humans"],
+   ["Booleans", "variable type that is true or false"],
+   ["Undefined", "When a variable has not been assigned a value"],
+   ["Parameter", "the names of the arguments in a function"],
+   ["Return", "the value provided back to the user after a function is performed"]
+ ];
+
+ });
